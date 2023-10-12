fa032198341fbcde63e1f8ac5c836c7ed49104c3
root <hkamau7129@gmail.com>
2023-10-11 20:00:55 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,16 @@
 #include "main.h"
 
+/**
+ * main - Check Code
+ *
+ * Description: Prints the alphabet in lowercase followed by a new line.
+ */
+void print_alphabet(void)
+{
+    char letter;
 
-	/**
-	 *  main - Check Code
-	 *
-	 * Description: Prints the alphabet in lowercase followed by a new line.
-	 * 
-	 */
+    for (letter = 'a'; letter <= 'z'; letter++)
+        _putchar(letter);
 
-
-	void print_alphabet(void)
-	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
-
-
-		_putchar('\n');
-	}		
+    _putchar('\n');
+}	
